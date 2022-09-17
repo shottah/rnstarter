f331dee191d254646d855e2f55e9933b3cb9c8c0
Matthew Abraham <wasteofintel@gmail.com>
2022-09-17 19:32:46 -0400

    feat: add redux, persistence, and saga

---
+++ src/index.d.ts
@@ -0,0 +1,9 @@
+declare module '*.json' {
+  const value: any;
+  export default value;
+}
+declare module '*.png';
+declare module '*.jpg';
+
+declare module 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+declare module 'redux-persist-fs-storage';
